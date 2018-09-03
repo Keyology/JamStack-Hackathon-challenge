fb26c5dcd5c969d2e4320e7b39b1ca62f309dfbe
Keoni Murray <keonimurray45@gmail.com>
2018-09-03 11:54:35 -0700

    Jamstack Hackathon

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello Team at freecode camp</h1>
+    <p>I am so excited to participate in the jamstack hackathon!!!!!</p>
+    <p>See you at jam stack!.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
---
+++ src/pages/page-2.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SecondPage = () => (
+  <div>
+    <h1>now let'code!!</h1>
+
+  <p> 2</p>
+<p><a href="https://github.com/Keyology">Check out my github!</a></p>
+
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default SecondPage
